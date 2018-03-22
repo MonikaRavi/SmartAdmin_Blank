46f21cc7daf073f840bc1f48e1f7c3f2b5742de7
Monika Ravi <monikar@hawsco.com>
2018-03-22 08:48:59 -0700

    server.js modified

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
